563d6f0a87a96f218f8c80345adb444784a5b840
Isaac Tait <isaac@mountaintopcoding.com>
2023-05-09 15:11:06 -0400

    fix: attempt 1 at header layout 🧇

--- new-grid-times/src/components/Header/Header.js
+++ new-grid-times/src/components/Header/Header.js
@@ -29,7 +29,22 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        {/* Menu layout for laptop and up */}
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
+        <ActionGroup>
+          <button>
+            <User size={24} />
+          </button>
+        </ActionGroup>
         <Logo />
+        <div>Subscribe</div>
       </MainHeader>
     </header>
   );
@@ -39,6 +54,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -50,10 +69,6 @@ const ActionGroup = styled.div`
   display: flex;
   gap: 24px;
 
-  /*
-    FIX: Remove the inline spacing that comes with
-    react-feather icons.
-  */
   svg {
     display: block;
   }
@@ -65,6 +80,13 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    grid-template-columns: 1fr, auto, 1fr;
+    justify-content: revert;
+    align-items: revert;
+  }
 `;
 
 export default Header;
